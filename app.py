4f5f338b687d3e77d4055f342ee01a2049d8aa33
Ryan Owen Thionanda <owenizedd@gmail.com>
2021-09-30 20:17:11 +0700

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 @app.route('/', methods=['POST'])
 def get_value():
     print('listnya', request.form.getlist('ingredient[]'))
-    print('value pertamanya', request.form.getlist('ingredient[]')[0])
+    print('value pertamanya', request.form.getlist('ingredient[]'))
     return render_template('pass.html')
 
 if __name__ == "__main__":
